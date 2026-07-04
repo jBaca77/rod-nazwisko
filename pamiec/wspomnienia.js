a63c55bb44a6eecd1b10522762c96e3b36bbe9d4
jBaca77 <j.nieweglowski96@gmail.com>
2026-07-04 22:42:37 +0200

    Create wspomnienia.js

---
+++ pamiec/wspomnienia.js
@@ -0,0 +1,38 @@
+const osoby = [
+    {
+        imie: "Andrzej Seweryn Kopacz",
+        lata: "1927-1975",
+        rokZgonu: 1975,
+        foto: "kopacz_andrzej.jpg"
+    },
+    {
+        imie: "Antoni Maurycy Walenty Kraiński",
+        lata: "1883-1975",
+        rokZgonu: 1975,
+        foto: "antoni_krainski.jpg"
+    },
+    {
+        imie: "Halina Krobicka zd. Kisiel",
+        lata: "1923-1975",
+        rokZgonu: 1975,
+        foto: "halina_krobicka.jpg"
+    },
+    {
+        imie: "Franciszka Waleria Ładomirska",
+        lata: "1897-1976",
+        rokZgonu: 1976,
+        foto: "waleria_ladomirska.jpg"
+    },
+    {
+        imie: "Janusz Głowacki",
+        lata: "1920-1976",
+        rokZgonu: 1976,
+        foto: "janusz_glowacki.jpg"
+    },
+    {
+        imie: "Marjorie Helene Caroline Ładomirska",
+        lata: "1908-1976",
+        rokZgonu: 1976,
+        foto: "marjorie_ladomirska.jpg"
+    }
+];
